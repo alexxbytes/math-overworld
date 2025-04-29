87b0dfcdb77ad2f9662f70b983481fe30b0a1c31
AlexxBytes <alexxbytes864@gmail.com>
2025-04-29 18:14:40 -0500

    Wipe

--- utils/compile/common.bat
+++
@@ -1,1 +0,0 @@
-g++ -fdiagnostics-color=always -g ../../src/main.cpp -o ../../bin/main.exe -lglfw3 -lopengl32 -lgdi32 -I ../../include -L ../../lib
--- utils/compile/windows.bat
+++
@@ -1,2 +0,0 @@
-g++ -fdiagnostics-color=always -g ../../src/main.cpp -o ../../bin/main.exe -lglfw3 -lopengl32 -lgdi32 -I ../../include -L ../../lib
-robocopy ..\..\lib\ ..\..\bin /E
